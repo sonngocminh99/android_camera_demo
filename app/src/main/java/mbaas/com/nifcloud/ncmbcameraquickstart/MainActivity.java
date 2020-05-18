709708c6e8063c11e7b114df15f43f6dd6d68de5
vfa.trucldt <vfa.trucldt@gmail.com>
2020-05-18 13:53:00 +0700

    Update application key

--- app/src/main/java/mbaas/com/nifcloud/ncmbcameraquickstart/MainActivity.java
+++ app/src/main/java/mbaas/com/nifcloud/ncmbcameraquickstart/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //**************** APIキーの設定とSDKの初期化 **********************
-        NCMB.initialize(this.getApplicationContext(), "YOUR_APP_KEY", "YOUR_CLIENT_KEY");
+        NCMB.initialize(this.getApplicationContext(), "YOUR_APPLICATION_KEY", "YOUR_CLIENT_KEY");
 
         setContentView(R.layout.activity_main);
 
